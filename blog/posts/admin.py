0da63c91c02a0837b809a40b2ba8b9f7d541ce0d
kropiak <kropiak@matman.uwm.edu.pl>
2025-02-22 16:07:00 +0100

    dodanie pierwszych modeli

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+# modele musimy zaimportować
+from .models import Category, Topic
+
+# a następnie zarejestrować (pokazano najprostszy przypadek)
+admin.site.register(Category)
+admin.site.register(Topic)
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=60)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
