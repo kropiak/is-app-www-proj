f02b6038ad003720bb3caf9ea9b1c39a1f2cd80a
kropiak <kropiak@matman.uwm.edu.pl>
2025-02-23 11:15:36 +0100

    koniec lab 4

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
 # modele musimy zaimportować
-from .models import Category, Topic
+from .models import Category, Topic, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ["created_at", "modified_at"]
 
 # a następnie zarejestrować (pokazano najprostszy przypadek)
 admin.site.register(Category)
 admin.site.register(Topic)
+admin.site.register(Post, PostAdmin)
 
 # C - CREATE
 # R - RETRIEVE
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -10,3 +11,12 @@ class Topic(models.Model):
     name = models.CharField(max_length=60)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+
+class Post(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
+    created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    # Shift + Alt + strzałki góra/dół - powielanie linii, w której znajduje się karetka
